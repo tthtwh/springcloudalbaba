674acdf76377528042e0f302f86fa1e2e2ded6c2
tangwenheng <125284809@qq.com>
2020-05-23 17:38:18 +0800

    奥术大师多

---
+++ global-demo/security/src/test/java/com/umi/demo/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umi.demo.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
